b1e874a3227a0db6323783fdcfe3dbf6f9ec8640
Simon Gomizelj <sgomizelj@sangoma.com>
2016-02-11 21:52:41 -0500

    Add pytest to setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
-from setuptools import setup
+import setuptools
 
 
-setup(name='safepy2',
-      version='1',
-      description='A python wrapper around the SAFe object model',
-      author='Simon Gomizelj',
-      author_email='sgomizelj@sangoma.com',
-      url='http://github.com/sangoma/safepy2',
-      packages = ['safe'],
-      install_requires=[
-          'six',
-          'requests'
-      ],
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Developers',
-                   'Operating System :: POSIX',
-                   'Topic :: Utilities',
-                   'Programming Language :: Python'])
+setup_params = dict(
+    name='safepy2',
+    version='1',
+    description='A python wrapper around the SAFe object model',
+    author='Simon Gomizelj',
+    author_email='sgomizelj@sangoma.com',
+    url='http://github.com/sangoma/safepy2',
+    packages=setuptools.find_packages(),
+    install_requires=['six', 'requests'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Intended Audience :: Developers',
+                 'Operating System :: POSIX',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3']
+)
+
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_params)
